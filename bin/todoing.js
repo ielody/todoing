9891fef20df6590716ad901a49d401ea91202747
Vidar Eldøy <vidar@eldoy.com>
2022-01-30 10:31:35 +0100

    Move run to bin, clean up a little

--- bin/todoing.js
+++ bin/todoing.js
@@ -1,5 +1,36 @@
 #!/usr/bin/env node
+const connection = require('mongowave')
 
-const todoing = require('../index.js')
+const api = require('../index.js')
 
-todoing()
+const command = process.argv[2]
+
+if (!command) {
+  console.log('Command not found.')
+  // TODO: print menu
+  // todo list
+  // todo create "Buy milk"
+  // todo update 1 "Buy butter"
+  // todo delete 1
+  // todo done 1
+  process.exit(0)
+}
+
+async function run(){
+  const db = await connection({ name: 'todoing' })
+
+  const fn = api[command]
+  if (typeof fn == 'function') {
+    await fn(db)
+  } else  {
+    console.log('Command not found.')
+  }
+  process.exit(0)
+}
+
+run()
+
+// Advanced debugging:
+// const obj = { a: 1, b: 2, c: 3, d: { f: { cuckoo: 5, world: 'hello'}} }
+// console.log(JSON.stringify(obj, null, 2))
+// console.log(obj)
--- index.js
+++ index.js
@@ -1,39 +1,6 @@
-const connection = require('mongowave')
-const { input } = require('extras')
-
 const create = require('./lib/create.js')
 const list = require('./lib/list.js')
 const remove = require('./lib/remove.js')
 const update = require('./lib/update.js')
 
-const api = { create, list, update, remove }
-
-const command = process.argv[2]
-
-if (!command) {
-  console.log('Command not found.')
-  // TODO: print menu
-  // todo list
-  // todo create "Buy milk"
-  // todo update 1 "Buy butter"
-  // todo delete 1
-  // todo done 1
-  process.exit(0)
-}
-
-module.exports = async function run(){
-  const db = await connection({ name: 'todo-ing' })
-
-  const fn = api[command]
-  if (typeof fn == 'function') {
-    await fn(db)
-  } else  {
-    console.log('Command not found.')
-  }
-  process.exit(0)
-}
-
-// Advanced debugging:
-// const obj = { a: 1, b: 2, c: 3, d: { f: { cuckoo: 5, world: 'hello'}} }
-// console.log(JSON.stringify(obj, null, 2))
-// console.log(obj)
+module.exports = { create, list, remove, update }
