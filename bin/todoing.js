9525fb31df79aa5b7dda74304db2db431379115a
Ingeborg Eldøy <ieldoy@gmail.com>
2022-02-09 19:32:04 +0100

    add farge, clean code

--- bin/todoing.js
+++ bin/todoing.js
@@ -9,15 +9,15 @@ const api = require('../index.js')
 
 let command = process.argv[2]
 
-if (command[0] == 'l') command = 'list'
-if (command[0] == 'c') command = 'create'
-if (command[0] == 'u') command = 'update'
-if (command[0] == 'r') command = 'remove'
-if (command[0] == 'd') command = 'done'
-if (command[0] == 'h') command = 'help'
+  if (command[0] == 'l') command = 'list'
+  if (command[0] == 'c') command = 'create'
+  if (command[0] == 'u') command = 'update'
+  if (command[0] == 'r') command = 'remove'
+  if (command[0] == 'd') command = 'done'
+  if (command[0] == 'h') command = 'help'
 
-if (!command) {
-  console.log(farge.red.log('Command not found.'))
+  else if (!command) {
+    farge.red.log('Command not found.')
   // TODO: print menu
   // todo list
   // todo create "Buy milk"
@@ -25,7 +25,7 @@ if (!command) {
   // todo remove 1
   // todo done 1
   process.exit(0)
-}
+ }
 
 async function run(){
   const db = await connection({ name: 'todoing' })
@@ -34,14 +34,9 @@ async function run(){
   if (typeof fn == 'function') {
     await fn(db)
   } else  {
-    console.log(farge.red.log('Command not found.'))
+    farge.red.log('Command not found.')
   }
   process.exit(0)
 }
 
 run()
-
-// Advanced debugging:
-// const obj = { a: 1, b: 2, c: 3, d: { f: { cuckoo: 5, world: 'hello'}} }
-// console.log(JSON.stringify(obj, null, 2))
-// console.log(obj)
