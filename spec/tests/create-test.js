86803d2623bb863643d7f344372a5ad6b547324e
Vidar Eldøy <vidar@eldoy.com>
2022-02-22 12:48:25 +0100

    Add new test for create

--- spec/tests/create-test.js
+++ spec/tests/create-test.js
@@ -15,3 +15,19 @@ it('should not create a new todo', async ({ t, db, state, mock, restore }) => {
   const count = await db('todo').count()
   t.ok(count === 0)
 })
+
+it('should create a new todo', async ({ t, db, state, mock, restore }) => {
+  state.inputs = ['hello']
+  mock()
+
+  await create(db)
+
+  restore()
+
+  t.ok(state.logs[0] == 'Add an item to your list\n')
+  t.ok(state.logs[1] == 'Enter task:\n')
+  t.ok(state.logs[2].includes('hello added'))
+
+  const count = await db('todo').count()
+  t.ok(count === 1)
+})
