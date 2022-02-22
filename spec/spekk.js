06bd07798980d5417adf774e7c6bb2dc79cfd486
Vidar Eldøy <vidar@eldoy.com>
2022-02-22 12:33:00 +0100

    Add create test

---
+++ spec/spekk.js
@@ -0,0 +1,42 @@
+const connection = require('mongowave')
+const extras = require('extras')
+
+const _write = process.stdout.write
+
+const state = {}
+function reset() {
+  state.count = 0
+  state.inputs = []
+  state.logs = []
+}
+
+extras.input = function() {
+  return state.inputs[state.count++]
+}
+
+module.exports = async function run () {
+
+  function mock() {
+    process.stdout.write = function(str) {
+      state.logs.push(str)
+    }
+  }
+
+  function restore() {
+    process.stdout.write = _write
+  }
+
+  // Set up db connection for example
+  const db = await connection({ name: 'todo-test' })
+
+  // Run before each test if defined
+  async function before() {
+    reset()
+    await db.drop()
+  }
+
+  // Run after each test if defined
+  async function after() {}
+
+  return { db, before, after, state, mock, restore }
+}
--- spec/tests/create-test.js
+++ spec/tests/create-test.js
@@ -1,10 +1,17 @@
-const task = require('../../lib/create.js')
-const connection = require('mongowave')
+const create = require('../../lib/create.js')
 
-test('creates a task object', async ({ t }) => {
-  const db = await connection({ name: 'todoing' })
-  await db('todo').create({ task })
-  t.ok(true)
-} )
+it('should not create a new todo', async ({ t, db, state, mock, restore }) => {
+  state.inputs = ['']
+  mock()
 
+  await create(db)
 
+  restore()
+
+  t.ok(state.logs[0] == 'Add an item to your list\n')
+  t.ok(state.logs[1] == 'Enter task:\n')
+  t.ok(state.logs[2].includes('Task not added. You need to enter at least 1 character.'))
+
+  const count = await db('todo').count()
+  t.ok(count === 0)
+})
