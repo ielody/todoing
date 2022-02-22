7036cfaa6e8e10c56cff01167bf3a0adce078f11
Vidar Eldøy <vidar@eldoy.com>
2022-02-22 12:54:15 +0100

    Create test with process argv

--- spec/tests/create-test.js
+++ spec/tests/create-test.js
@@ -16,7 +16,7 @@ it('should not create a new todo', async ({ t, db, state, mock, restore }) => {
   t.ok(count === 0)
 })
 
-it('should create a new todo', async ({ t, db, state, mock, restore }) => {
+it('should create a new todo with input', async ({ t, db, state, mock, restore }) => {
   state.inputs = ['hello']
   mock()
 
@@ -27,6 +27,22 @@ it('should create a new todo', async ({ t, db, state, mock, restore }) => {
   t.ok(state.logs[0] == 'Add an item to your list\n')
   t.ok(state.logs[1] == 'Enter task:\n')
   t.ok(state.logs[2].includes('hello added'))
+
+  const todo = await db('todo').get()
+  t.ok(todo.task == 'hello')
+})
+
+
+it('should create a new todo with args', async ({ t, db, state, mock, restore }) => {
+  process.argv[3] = 'hello'
+  mock()
+
+  await create(db)
+
+  restore()
+
+  t.ok(state.logs[0] == 'Add an item to your list\n')
+  t.ok(state.logs[1].includes('hello added'))
 
   const todo = await db('todo').get()
   t.ok(todo.task == 'hello')
