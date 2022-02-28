0934a8b4c314866b36c317120b1aa3fa30f43787
Ingeborg Eldøy <ieldoy@gmail.com>
2022-02-28 16:47:26 +0100

    add range, test list

--- spec/tests/list-test.js
+++ spec/tests/list-test.js
@@ -34,3 +34,46 @@ it('should list tasks', async ({ t, db, state, mock, restore }) => {
   const count = await db('todo').count()
   t.ok(count === 2)
 })
+
+it('should list specific tasks', async ({ t, db, state, mock, restore }) => {
+
+  await db('todo').create({ task: 'banana' })
+  await db('todo').create({ task: 'apple' })
+
+  process.argv[3] = '1'
+
+  mock()
+
+  await list(db)
+
+  restore()
+
+  t.ok(Object.keys(state.logs).length === 2)
+  t.ok(state.logs[0] == 'Your tasks\n')
+  t.ok(state.logs[1].includes('banana'))
+
+  const count = await db('todo').count()
+  t.ok(count === 2)
+})
+
+it('should list last task', async ({ t, db, state, mock, restore }) => {
+
+  await db('todo').create({ task: 'banana' })
+  await db('todo').create({ task: 'apple' })
+
+  process.argv[3] = '-1'
+
+  mock()
+
+  await list(db)
+
+  restore()
+
+  t.ok(Object.keys(state.logs).length === 2)
+  t.ok(state.logs[0] == 'Your tasks\n')
+  t.ok(state.logs[1].includes('apple'))
+
+  const count = await db('todo').count()
+  t.ok(count === 2)
+})
+
