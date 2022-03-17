16ea5be8d173126f7b3fa51d3447da60263bd6a3
Ingeborg Eldøy <ieldoy@gmail.com>
2022-03-17 16:29:46 +0100

    filter one word tasks

--- spec/tests/filter-test.js
+++ spec/tests/filter-test.js
@@ -1,7 +1,7 @@
 const filter = require('../../lib/filter.js')
 
-o('should list empty tasks', async ({ t, db, state, mock, restore }) => {
-
+it('should list empty tasks', async ({ t, db, state, mock, restore }) => {
+  state.inputs = ['']
   mock()
 
   await filter(db)
@@ -11,10 +11,30 @@ o('should list empty tasks', async ({ t, db, state, mock, restore }) => {
   t.ok(Object.keys(state.logs).length === 1)
   t.ok(state.logs[0] == "You don't have any tasks.\n")
 
-
   const count = await db('todo').count()
   t.ok(count === 0)
 })
 
 
+it('should take word from input and list tasks', async ({ t, db, state, mock, restore }) => {
 
+  await db('todo').create({ task: 'coffee' })
+  await db('todo').create({ task: 'tea' })
+
+  process.argv[3] = 'coffee'
+
+  mock()
+
+  await filter(db)
+
+  restore()
+
+  t.ok(Object.keys(state.logs).length === 1)
+  t.ok(state.logs[0] == 'Find tasks containing your chosen word\n')
+
+  const filterWord = await db('todo').find({ task: 'coffee' } )
+  console.log(filterWord)
+  t.ok(filterWord.length == 1)
+  t.ok(filterWord[0].task == 'coffee')
+
+})
